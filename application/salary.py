3fadd31b2cc944e439ebfe63b4438d4e9c0a6052
Vladislav Proshchenkov <vladislove.pro@gmail.com>
2024-07-02 23:20:56 +0300

    initial commit

---
+++ application/salary.py
@@ -0,0 +1,2 @@
+def calculate_salary():
+    print(f'Модуль {__name__} вычисляет оклад сотрудника.')
---
+++ main.py
@@ -0,0 +1,9 @@
+import datetime
+from application.salary import calculate_salary
+from application.db.people import get_employees
+
+
+if __name__ == '__main__':
+    print(datetime.datetime.date(datetime.datetime.now()))
+    calculate_salary()
+    get_employees()
